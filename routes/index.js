742c6a05dc05cc0d9427bbd3d30ef1878e0ef40c
suinbo <suin9610@gmail.com>
2024-04-26 13:09:49 +0900

    (test): 쿼리 파람 추가

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,14 @@ const router = express.Router()
 // 로그인
 router.get("/login", async (req, res) => {
     try {
-        const response = await axios.get("https://kauth.kakao.com/oauth/authorize", {})
+        const response = await axios.get("https://kauth.kakao.com/oauth/authorize", {
+            data: {
+                client_id: "304c6bc5d7275c6f37ccd6bf08550bb2",
+                redirect_uri: "http://localhost:5173/ourmap.github.io",
+                response_type: "code",
+            },
+        })
+        console.log("req:: ", req)
         return response
     } catch (error) {
         console.log(error.message)
