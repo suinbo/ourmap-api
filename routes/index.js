4de75e6a0c23bb2606db1142e7b811eda410ebbf
suinbo <suin9610@gmail.com>
2024-04-26 14:00:38 +0900

    (test) : test

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,16 @@ const router = express.Router()
 // 로그인
 router.get("/login", async (req, res) => {
     try {
-        const response = await axios.get("https://kauth.kakao.com/oauth/authorize", {
-            params: {
-                client_id: "304c6bc5d7275c6f37ccd6bf08550bb2",
-                redirect_uri: "http://localhost:5173",
-                response_type: "code",
-            },
-        })
+        const response = await axios.get(
+            "https://kauth.kakao.com/oauth/authorize?client_id=304c6bc5d7275c6f37ccd6bf08550bb2&redirect_uri=http://localhost:5173&response_type=code",
+            {
+                // params: {
+                //     client_id: "304c6bc5d7275c6f37ccd6bf08550bb2",
+                //     redirect_uri: "http://localhost:5173",
+                //     response_type: "code",
+                // },
+            }
+        )
         console.log("req:: ", req.body)
         return response
     } catch (error) {
