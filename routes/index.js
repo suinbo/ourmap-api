6a7add9763c35ed6dde4d5ada260d3fbe48ab038
suinbo <suin9610@gmail.com>
2024-04-29 09:16:56 +0900

    (test) : test

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,13 @@ const router = express.Router()
 // 로그인
 router.post("/getKakao", async (req, res) => {
     try {
-        console.log("headers:: ", req.headers)
-        console.log("body:: ", req.body)
-
         await axios
             .post(
                 "https://kauth.kakao.com/oauth/token",
                 {
                     grant_type: "authorization_code",
                     client_id: "304c6bc5d7275c6f37ccd6bf08550bb2",
-                    redirect_uri: "http://localhost:5173/ourmap.github.io/auth",
+                    redirect_uri: "http://localhost:5173/ourmap.github.io",
                     code: req.body.code,
                 },
                 {
